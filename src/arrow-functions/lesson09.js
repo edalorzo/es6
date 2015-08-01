288f980e8fae61e71b89713823bd33fe421753ba
Edwin Dalorzo <edwin@dalorzo.com>
2015-08-01 11:38:15 -0600

    Adds even more arrow function lessons

---
+++ src/arrow-functions/lesson09.js
@@ -0,0 +1,13 @@
+/*---------------------------------------------------
+ * IIFE with Arrow Functions
+ *--------------------------------------------------*/
+
+
+ (function IIFE(){
+ 	console.log('Hello from IIFE');
+ })();
+
+
+ (() => {
+ 	console.log('Hello Again');
+ })();
